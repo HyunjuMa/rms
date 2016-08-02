b196b4143487a6be28d69600ab6575226d171898
hyunjuma <flipped71@gmail.com>
2016-08-02 09:29:13 +0900

    dbtest

--- routes/auth/register/index.js
+++ routes/auth/register/index.js
@@ -1,6 +1,6 @@
 //회원 가입과 관련된 라우팅
 var express = require('express');
-var router = express.Router();
+var app = express();
 
 //Controller 관리
 var register = require('../../../controllers/auth/register');
@@ -8,26 +8,28 @@ var register = require('../../../controllers/auth/register');
 //Common 관리
 var common = require('../../../controllers/common');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('auth/register', { title: 'Register Page' });
-});
+module.exports = function(router, User) {
 
-router.post('auth/reqister', function(req,res){
-  var user = new User();
-  user.name = req.body.name;
-  user.email = req.body.email;
-  user.pw = req.body.pw;
-  user.imgId = req.body.image;
-
-  user.save(function(err) {
-    if(err) {
-      console.error(err);
-      res.json({result: 0});
-      return;
-    }
-    res.json({result: 1});
+  /* GET home page. */
+  app.get('/', function(req, res, next) {
+    res.render('auth/register', { title: 'Register Page' });
   });
-});//create user
 
-module.exports = router;
+  app.post('auth/reqister/', function(req,res){
+    var user = new User();
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.pw = req.body.pw;
+    user.imgId = req.body.imgId;
+
+    user.save(function(err) {
+      if(err) {
+        console.error(err);
+        res.json({result: 0});
+        return;
+      }
+      res.json({result: 1});
+    });
+  });//create user
+
+}
